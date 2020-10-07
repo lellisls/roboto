0ef69a9e6c7263fd205ec59b773e2907cb6a8b9f
Lucas Lellis <lucaslellis777@gmail.com>
2020-10-07 19:51:07 -0300

    Adding pwm pins

--- run.py
+++ run.py
@@ -3,54 +3,62 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+pwm1 = 13
 p1 = 18
 p2 = 17
+
+pwm2 = 12
 p3 = 27
 p4 = 22
 
 class Motor:
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2, pwm):
         self.p1 = p1
         self.p2 = p2
+        self.pwm_pin = pwm
         GPIO.setup(self.p1, GPIO.OUT)
         GPIO.setup(self.p2, GPIO.OUT)
-        self.pwm = None
-    
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pwm_pin, 100)
+
     def stop(self):
-        if self.pwm is not None:
-            self.pwm.stop()
-            self.pwm = None
         GPIO.output(self.p1, GPIO.LOW)
         GPIO.output(self.p2, GPIO.LOW)
+        self.pwm.stop()
 
     def run(self, speed: float, direction: float):
-        self.stop()
-        if direction != 0 and speed != 0:
-            speed_pin = self.p1 if direction > 0 else self.p2
-            direction_pin = self.p2 if direction > 0 else self.p1
-            self.pwm = GPIO.PWM(speed_pin, 100)
-            GPIO.output(direction_pin, GPIO.LOW)
-            self.pwm.start(speed)
+        p1_signal = GPIO.HIGH if direction > 0 else GPIO.LOW
+        p2_signal = GPIO.LOW if direction > 0 else GPIO.HIGH
+        GPIO.output(self.p1, p1_signal)
+        GPIO.output(self.p2, p2_signal)
+        self.pwm.start(speed)
 
 def run():
-    m1 = Motor(p1, p2)
-    m2 = Motor(p3, p4)
-    speed = 100
+    m1 = Motor(p1, p2, pwm1)
+    m2 = Motor(p3, p4, pwm2)
+    speed = 40
 
-    m1.run(speed, 1)
-    m2.run(speed, -1)
+    FW = 1
+    BW = -1
+    ST = 0
 
-    time.sleep(5)
+    commands = [
+        (FW,FW, 1),
+        (BW,BW, 1),
+        (FW,BW, 1),
+        (FW,FW, 1),
+        (BW,FW, 1),
+        (FW,FW, 1),
+        (BW,FW, 1),
+        (FW,FW, 1),
+    ]
 
-    m1.stop()
-    m2.stop()
+    while True:
+        for (dir1, dir2, sleep_time) in commands:        
+            m1.run(speed, dir1)
+            m2.run(speed, dir2)
 
-    time.sleep(.1)
-
-    m1.run(speed, -1)
-    m2.run(speed, 1)
-
-    time.sleep(5)
+            time.sleep(sleep_time)
 
 try:
     run()
